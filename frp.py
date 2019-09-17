abfef64c616f825babb33b1c63b5d4bf371bedb4
Marisa Woo <35637865+marisawoo@users.noreply.github.com>
2019-09-17 18:20:09 -0400

    Update frp.py

--- frp.py
+++ frp.py
@@ -2,13 +2,13 @@
 # E(F) : set of F-rational points on elliptic curve E
 # E : Y^2 = X^3 + 8X + 15
 
-def findSet(p):
+def findSet(p, a, b):
     frp = [0]
     for y in range(p):
         for x in range(p):
-            ok = (y**2)%p
-            hello = (x**3 + 8*x + 15)%p
-            if (ok == hello):
+            lhs = (y**2)%p
+            rhs = (x**3 + a*x + b)%p
+            if (lhs == rhs):
                 frp.append((x, y))
     print("cardinality: {0}".format(len(frp)))
     return frp
