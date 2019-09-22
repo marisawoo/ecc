1717261bb51fc05918f97448ff8ccaa860ad0d69
Marisa Woo <35637865+marisawoo@users.noreply.github.com>
2019-09-22 01:56:48 -0400

    Update frp.py

--- frp.py
+++ frp.py
@@ -2,19 +2,19 @@
 # E(F) : set of F-rational points on elliptic curve E
 # E : Y^2 = X^3 + AX + B
 
-def findSet(p, a, b):
+def findSet(prime, a, b):
     frp = [0]
-    for y in range(p):
-        for x in range(p):
-            lhs = (y**2)%p
-            rhs = (x**3 + a*x + b)%p
+    for y in range(prime):
+        for x in range(prime):
+            lhs = (y**2)%prime
+            rhs = (x**3 + a*x + b)%prime
             if (lhs == rhs):
                 frp.append((x, y))
     print("cardinality: {0}".format(len(frp)))
     return frp
 
-def findInverse(b, p):
-    for i in range(p):
+def findInverse(b, prime):
+    for i in range(prime):
         if (b*i)%p == 1:
             return i
     print("Did not find inverse")
